6072eeef67ef932db505fb37fc49a14b771fd7d5
Anish Mandal <anish.mandal@avishkaram.com>
2023-09-06 09:29:51 +0545

    Add project files.

---
+++ Domain/Orders/Order.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Domain.Customers;
+using Domain.Products;
+
+namespace Domain.Orders;
+
+public class Order
+{
+
+    private Order()
+    {
+    }
+    private readonly HashSet<LineItem> _lineItems = new();
+    public Guid Id { get; private set; }
+    public Guid CustomerId { get; private set; }
+
+
+    public static Order Create(Customer customer)
+    {
+        var order = new Order
+        {
+            Id = Guid.NewGuid(),
+            CustomerId = customer.Id
+        };
+        return order;
+    }
+
+    public void Add(Product product)
+    {
+        var LineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+        _lineItems.Add(LineItem);
+    }
+}
+
+
+public class LineItem
+{
+
+    internal LineItem(Guid id, Guid orderId, Guid productId, Money price)
+    {
+        Id = id;
+        OrderId = orderId;
+        ProductId = productId;
+        Price = price;
+    }
+    public Guid Id { get; private set; }
+
+    public Guid OrderId { get; private set; }
+
+    public Guid ProductId { get; private set; }
+
+    public Money Price { get; private set; }
+}
+
---
+++ Domain/Products/Sku.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Products;
+
+
+// Stock Keeping Unit
+//Factory Method
+public record Sku
+{
+    private const int DefaultLength = 15;
+
+    private Sku(string value) => Value = value;
+
+    public string Value { get; init; } // init -- as soon as the value is set it is unmodifiable
+
+    public static Sku? Create(string value)
+    {
+        if (string.IsNullOrEmpty(value)) return null;
+
+        if (value.Length != DefaultLength) return null;
+
+        return new Sku(value);
+    }
+}
